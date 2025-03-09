51689eff38b1665bf9189b2a4963761e1fc7a691
Justin Trahan <justin.m.trahan@outlook.com>
2025-03-09 18:47:02 +0000

    add update struct parser

--- cmd/dns_req_structs.go
+++ cmd/dns_req_structs.go
@@ -11,6 +11,7 @@ type DnsRecordModRequest struct {
 }
 
 type DnsRecord struct {
+	ID         string    `yaml:"id" json:"id"`
 	ZoneName   string    `yaml:"zone_name" json:"zone_name"`
 	Name       string    `yaml:"name" json:"name"`
 	Content    string    `yaml:"content" json:"content"`
@@ -18,12 +19,13 @@ type DnsRecord struct {
 	TTL        int       `yaml:"ttl" json:"ttl"`
 	Proxied    bool      `yaml:"proxied" json:"proxied"`
 	Comment    string    `yaml:"comment" json:"comment"`
+	Priority   *uint16   `yaml:"priority" json:"priority"`
 	Tags       []string  `yaml:"tags" json:"tags"`
 	ModifiedOn time.Time `yaml:"modified" json:"modified"`
 	CreatedOn  time.Time `yaml:"created" json:"created"`
 }
 
-func (cr *DnsRecord) ParseToCloudflareCreateParam() *cloudflare.CreateDNSRecordParams {
+func (cr *DnsRecord) ParseToCloudflareCreateParams() *cloudflare.CreateDNSRecordParams {
 	cfreq := &cloudflare.CreateDNSRecordParams{
 		Name:       cr.Name,
 		Content:    cr.Content,
@@ -32,8 +34,25 @@ func (cr *DnsRecord) ParseToCloudflareCreateParam() *cloudflare.CreateDNSRecordP
 		Proxied:    &cr.Proxied,
 		Comment:    cr.Comment,
 		Tags:       cr.Tags,
+		Priority:   cr.Priority,
 		ModifiedOn: cr.ModifiedOn,
 		CreatedOn:  cr.CreatedOn,
+	}
+
+	return cfreq
+}
+
+func (cr *DnsRecord) ParseToCloudflareUpdateParams() *cloudflare.UpdateDNSRecordParams {
+	cfreq := &cloudflare.UpdateDNSRecordParams{
+		ID:       cr.ID,
+		Name:     cr.Name,
+		Content:  cr.Content,
+		TTL:      cr.TTL,
+		Type:     cr.ZoneName,
+		Proxied:  &cr.Proxied,
+		Comment:  &cr.Comment,
+		Tags:     cr.Tags,
+		Priority: cr.Priority,
 	}
 
 	return cfreq
