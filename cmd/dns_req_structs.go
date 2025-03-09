f1d6069ab4333140d092f0670e3d3ba44c82c57e
Justin Trahan <justin.m.trahan@outlook.com>
2025-03-09 18:23:14 +0000

    added parse methods

--- cmd/dns_req_structs.go
+++ cmd/dns_req_structs.go
@@ -1,6 +1,10 @@
 package cmd
 
-import "time"
+import (
+	"time"
+
+	"github.com/cloudflare/cloudflare-go"
+)
 
 type DnsRecordModRequest struct {
 	NewOrModifiyRecords map[string]DnsRecord `yaml:"zone_id" json:"zone_id"`
@@ -9,6 +13,7 @@ type DnsRecordModRequest struct {
 type DnsRecord struct {
 	ZoneName string    `yaml:"zone_name" json:"zone_name"`
 	Name     string    `yaml:"name" json:"name"`
+	Content  string    `yaml:"content" json:"content"`
 	Type     string    `yaml:"type" json:"type"`
 	TTL      int       `yaml:"ttl" json:"ttl"`
 	Proxied  bool      `yaml:"proxied" json:"proxied"`
@@ -16,4 +21,20 @@ type DnsRecord struct {
 	Tags     []string  `yaml:"tags" json:"tags"`
 	Modified time.Time `yaml:"modified" json:"modified"`
 	Created  time.Time `yaml:"created" json:"created"`
+}
+
+func (cr *DnsRecord) ParseToCloudflareCreateParam() *cloudflare.CreateDNSRecordParams {
+	cfreq := &cloudflare.CreateDNSRecordParams{
+		Name:       cr.Name,
+		Content:    cr.Content,
+		TTL:        cr.TTL,
+		Type:       cr.ZoneName,
+		Proxied:    &cr.Proxied,
+		Comment:    cr.Comment,
+		Tags:       cr.Tags,
+		ModifiedOn: cr.Modified,
+		CreatedOn:  cr.Created,
+	}
+
+	return cfreq
 }
