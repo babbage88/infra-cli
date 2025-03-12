34ddde9682d824ed184c9ab2d7b6b48f6b589eea
jtrahan <justin@trahan.dev>
2025-03-12 10:48:17 -0500

    tested dns-config and records parsing.

--- cmd/dns.go
+++ cmd/dns.go
@@ -5,17 +5,26 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var recordsBatch DnsRecordBatchRequest
+
 var dnsCmd = &cobra.Command{
 	Use:   "dns",
 	Short: "DNS management commands",
 }
 
 func init() {
+
 	rootCmd.AddCommand(dnsCmd)
 	dnsCmd.PersistentFlags().StringVar(&dnsCfgFile, "dns-config", "",
-		"Config file (default is default.yaml)")
-	err := mergeDnsConfigFile()
-	if err != nil {
-		pretty.PrintErrorf("error merging dns-config %s", err.Error())
-	}
+		"Config file (default is dns-config.yaml)")
+
+	cobra.OnInitialize(func() {
+		if dnsCfgFile != "" {
+			err := mergeDnsConfigFile()
+			if err != nil {
+				pretty.PrintErrorf("error merging dns-config %s", err.Error())
+			}
+		}
+
+	})
 }
---
+++ cmd/dns_cloudflare.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"github.com/babbage88/infra-cli/internal/pretty"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cloudflareCmd = &cobra.Command{
+	Use:   "cloudflare",
+	Short: "Manage Cloudflare DNS records",
+	Run: func(cmd *cobra.Command, args []string) {
+		for _, record := range recordsBatch.Records {
+			pretty.Print("The following records were parsed from dns-config:\n")
+			//pretty.Printf("API Token for cloudflare: %s", rootCfg.GetStringMapString("api_tokens")["cloudflare"])
+
+			pretty.Printf("ZoneName: %s", record.ZoneName)
+			pretty.Printf("Name: %s", record.Name)
+			pretty.Printf("Content: %s", record.Content)
+		}
+	},
+}
+
+var (
+	cfDnsToken string
+)
+
+func init() {
+	dnsCmd.AddCommand(cloudflareCmd)
+	//Checking if api token for cloudflare was passed via apiTokens from root command.
+	cobra.OnInitialize(func() {
+		apiTokens = viper.GetStringMapString("api_tokens")
+		cfapi, exists := apiTokens["cloudflare"]
+		if exists {
+			cfDnsToken = cfapi
+		} else {
+			viper.SetEnvPrefix("cf")
+			viper.AutomaticEnv()
+		}
+
+		if dnsCfgFile != "" {
+			if err := dnsCfg.Unmarshal(&recordsBatch); err != nil {
+				pretty.PrintErrorf("Unable to decode into struct: %v", err)
+			}
+		}
+
+	})
+
+}
