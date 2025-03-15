bdf5d3bf16b18118353799f1aa357a88a9380229
Justin Trahan <justin@trahan.dev>
2025-03-15 13:07:39 -0500

    Added length flag param for jwt secret gen

--- cmd/jwt_generate_secret.go
+++ cmd/jwt_generate_secret.go
@@ -14,7 +14,8 @@ var generateCmd = &cobra.Command{
 	Use:   "generate-secret",
 	Short: "Generate an HMAC256 secret",
 	Run: func(cmd *cobra.Command, args []string) {
-		secret := make([]byte, 32)
+		length := viper.GetInt64("secret_length")
+		secret := make([]byte, length)
 		_, err := rand.Read(secret)
 		if err != nil {
 			fmt.Println("Look who finally decided to showed up! :", err)
@@ -50,12 +51,15 @@ func init() {
 	generateCmd.Flags().StringVarP(&jsonFile, "output-json-file", "j", "", "Write secret to .json file")
 	generateCmd.Flags().StringVarP(&tomlFile, "output-toml-file", "t", "", "Write secret to .toml file")
 	generateCmd.Flags().StringVarP(&envFile, "output-env-file", "e", "", "Write secret to .env file")
-	generateCmd.Flags().StringVarP(&jwtKeyName, "jwt-key-name", "k", "JWT_KEY", "Key name for JWT secret in .env file")
+	generateCmd.Flags().StringVar(&jwtKeyName, "jwt-key-name", "JWT_KEY", "Key name for JWT secret in .env file")
 	generateCmd.Flags().StringVarP(&jwtTokenName, "jwt-token-name", "a", "JWT_AUTH_TOKEN", "Key name for JWT tokens in .env file")
+	generateCmd.Flags().Int64("secret-length", 64, "The number of random characters to use for the secrest's length")
 
 	// Bind Viper config
 	viper.SetDefault("jwt_key_name", "JWT_KEY")
 	viper.SetDefault("jwt_token_name", "JWT_AUTH_TOKEN")
+	viper.SetDefault("secret_length", int64(64))
+	viper.BindPFlag("secret_length", generateCmd.Flags().Lookup("secret-length"))
 	viper.BindPFlag("jwt_key_name", generateCmd.Flags().Lookup("jwt-key-name"))
 	viper.BindPFlag("jwt_token_name", generateCmd.Flags().Lookup("jwt-token-name"))
 	viper.BindPFlag("output_yaml_file", generateCmd.Flags().Lookup("output-yaml-file"))
